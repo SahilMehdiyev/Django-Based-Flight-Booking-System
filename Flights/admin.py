02e4b621d496515d1944aa0915c4071634da2391
SahilMehdiyev <branchsahil@gmail.com>
2024-05-31 12:41:27 +0400

    admin panel ozellestirildi

--- Flights/admin.py
+++ Flights/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Flight
 
-admin.site.register(Flight)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('departure_city','arrivel_city','departure_date','arrivel_date','airline','price')
+    list_filter = ('departure_city',)
+    search_fields = ('departure_city',)
+    ordering = ('departure_date',)
+    
+
+admin.site.register(Flight,FlightAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'first_name','last_name')
+    list_filter = ('is_active',)
+    ordering = ('username',)
+    search_fields = ('username',)
+    
+
+
+admin.site.register(User,UserAdmin)
 
 
 
